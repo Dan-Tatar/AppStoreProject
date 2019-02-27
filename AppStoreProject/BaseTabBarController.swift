0a4823a9c3b032ad4663780a68781eb6b62bec73
Dan Tatar <dan.tatar@yahoo.com>
2019-02-27 19:02:40 +0000

    Create BaseTabBarController that holds the viewControllers

---
+++ AppStoreProject/BaseTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreProject
+//
+//  Created by Dan  Tatar on 26/02/2019.
+//  Copyright © 2019 Dany. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController : UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+        
+        viewControllers = [createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+                           createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+                           createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+                          ]
+    }
+    // Function that creates navigation controllers to be added to the viewControllers array
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.view.backgroundColor = UIColor.white
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        navController.navigationBar.prefersLargeTitles = true
+        return navController
+    }
+}
