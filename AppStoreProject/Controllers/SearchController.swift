b94e8483ca0afec915317a00d7ec78d4e8ce551d
Dan Tatar <dan.tatar@yahoo.com>
2019-03-08 09:53:30 +0000

    Creating data model and adding function to fetch data.

--- AppStoreProject/Controllers/SearchController.swift
+++ AppStoreProject/Controllers/SearchController.swift
@@ -11,12 +11,33 @@ import UIKit
 class SearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     fileprivate let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
         collectionView.register(SearchResultsCell.self, forCellWithReuseIdentifier: cellID)
+        fetchITunesData()
+    }
+    
+    func fetchITunesData() {
         
+        let urLString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urLString) else { return}
+        URLSession.shared.dataTask(with: url) { (data, response, error)  in
+            
+            if let error = error {
+                print("Failed to fetch Apps", error)
+            }
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let searchResult = try decoder.decode(SearchResult.self, from: data)
+                searchResult.results.forEach( { print($0.trackName, $0.primaryGenreName)} )
+            } catch let jsonError {
+                print("Failed to decode json", jsonError)
+            }
+            }.resume()
     }
     
     init() {
@@ -25,7 +46,7 @@ class SearchController: UICollectionViewController, UICollectionViewDelegateFlow
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! SearchResultsCell
-
+        cell.nameLabel.text = "Here is my App name"
         return cell
     }
     
