ea837345644beb059504ef7460ec55a2dc7a4181
Dan Tatar <dan.tatar@yahoo.com>
2019-07-10 20:48:14 +0100

    Add TodayController and create collectionView cells.

---
+++ AppStoreProject/Views/TodayFeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  TodayFeedCell.swift
+//  AppStoreProject
+//
+//  Created by Dan  Tatar on 07/07/2019.
+//  Copyright © 2019 Dany. All rights reserved.
+//
+
+import UIKit
+
+class TodayCell: UICollectionViewCell {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
