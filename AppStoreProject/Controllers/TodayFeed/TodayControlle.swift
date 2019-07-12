1927e5fcc8df175a0b40aa44fac8bbc1e0106bc5
Dan Tatar <Dan.Tatar@rclighting.com>
2019-07-12 10:29:59 +0100

    WIP - Add animation  for view to expand when pressing on today cell

--- AppStoreProject/Controllers/TodayFeed/TodayControlle.swift
+++ AppStoreProject/Controllers/TodayFeed/TodayControlle.swift
@@ -17,11 +17,12 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.isNavigationBarHidden = true
         collectionView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: todayCell)
         
-    }
+    }  
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: todayCell, for: indexPath)
@@ -33,7 +34,38 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("Animate")
+       
+        let redView = UIView()
+        redView.backgroundColor = .red
+        redView.layer.cornerRadius = 16
+        
+        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
+        guard let cell = collectionView.cellForItem(at: indexPath) else {
+            return
+        }
+       
+        // absolute coordinate of frame
+        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
+        redView.frame = startingFrame
+        view.addSubview(redView)
+        
+        self.startingFrame = startingFrame
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            redView.frame = self.view.frame
+        }, completion: nil)
+    }
+    
+    var startingFrame: CGRect?
+    
+    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            gesture.view?.frame = self.startingFrame ?? .zero
+        }, completion: { _ in
+            gesture.view?.removeFromSuperview()
+           }
+        )
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
