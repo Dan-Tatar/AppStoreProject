ac4029913bf67739131303288ce834c705084f5f
Dan Tatar <Dan.Tatar@rclighting.com>
2019-07-16 10:15:49 +0100

    Fix bug to animate correctly by changing the constraints.

--- AppStoreProject/Controllers/TodayFeed/TodayControlle.swift
+++ AppStoreProject/Controllers/TodayFeed/TodayControlle.swift
@@ -35,6 +35,11 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     
     var animationTransitionController: UIViewController!
     
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
+    
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
        
         let animationTransitionController = AnimationTransitionController()
@@ -51,7 +56,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
        
         // absolute coordinate of frame
         guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
-        redView.frame = startingFrame
+//        redView.frame = startingFrame
         view.addSubview(redView)
         
         // calling this for the view to render itself and call the methods from animationTransitionController
@@ -61,9 +66,24 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         
         self.startingFrame = startingFrame
         
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+        
+        [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
+        self.view.layoutIfNeeded()
+        
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
-            redView.frame = self.view.frame
             
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
+            self.view.layoutIfNeeded()
+        
             self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
         }, completion: nil)
     }
@@ -73,17 +93,27 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
-            gesture.view?.frame = self.startingFrame ?? .zero
-             self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 0)
+           
+            guard let startingFrame = self.startingFrame else { return }
+            
+            self.topConstraint?.constant = startingFrame.origin.y
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
+            
+            self.view.layoutIfNeeded()
+            
+            self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 0)
+            
         }, completion: { _ in
-            gesture.view?.removeFromSuperview()
+            self.animationTransitionController.view.removeFromSuperview()
             self.animationTransitionController.removeFromParent()
            }
         )
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width - 64, height: 400)
+        return .init(width: view.frame.width - 64, height: 450)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
